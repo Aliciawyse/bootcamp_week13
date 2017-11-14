e65db00591c8e7af68f34b708c835876ce663d54
Barrett <Aliciawyse@users.noreply.github.com>
2017-11-14 18:16:24 -0500

    wip

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -7,17 +7,14 @@ var burger = require('../models/burger.js');
 
 // Create all our routes and set up logic within those routes where required.
 router.get("/", function(req, res) {
-    makeBurger.all(function(data) {
+    burger.selectAll(function(data) {
         var burgerObject = {
-            burger: data
+            burgers: data
         };
         console.log(burgerObject);
         res.render("index", burgerObject);
     });
 });
-
-
-
 
 // Export routes for server.js to use.
 module.exports = router;
